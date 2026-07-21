bee3f987e7600706f37b74810d85b8169613e048
enyonamklutse <enyoklutse@gmail.com>
2026-07-21 11:50:14 -0400

    added javascript to my work

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let student_name=prompt("WHAT IS YOUR NAME?")
+let student_age=prompt("PLEASE TYPE YOUR AGE?")
+let student_email=prompt("WHAT IS YOUR EMAIL?")
+
+let message= alert(`Hi ${student_name}, you are ${student_age} years old and we'll keep in touch via your email: ${student_email}`)
